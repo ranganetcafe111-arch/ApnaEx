3c90a41b7bfeb397ed2e940d1e51f1fd76fd8af0
ranganetcafe111-arch <ranganetcafe111@gmail.com>
2026-03-15 17:35:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,24 +5,24 @@ from os import getenv
 # ------------------------------------------------
 API_ID = int(os.environ.get("API_ID", "33170551"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","60c6aela7e96884d0672a05e0b7633bf")
+API_HASH = os.environ.get("API_HASH","60c6ae1a7e96884d0672a05e0b7633bf")
 # ------------------------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Chartkaryi_bot")
-BOT_TEXT = "GAJJU BHAI KA EXTRACTOR"
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@arjunbhai_extractor_bot")
+BOT_TEXT = "KARTHI BHAI EXTRACTOR"
 # ------------------------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "1910159009"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100388527503"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003777023181"))
 
 # //FORCE_CHANNEL_ID
 CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002195125473")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://besib69802:YMOfgvnyjbRgW5qt@cluster0.yzzu2gn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002195125473"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003777023181"))
 # -----------------------------------------------
 join = '<a href="https://t.me/RPSC_UPSC_CURRENT">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
