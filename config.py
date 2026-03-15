724d758e447b3363ad88fbcd3d12447c4d5c188f
ranganetcafe111-arch <ranganetcafe111@gmail.com>
2026-03-15 17:44:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1910159009"))
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003777023181"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002195125473")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003777023181")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://besib69802:YMOfgvnyjbRgW5qt@cluster0.yzzu2gn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
